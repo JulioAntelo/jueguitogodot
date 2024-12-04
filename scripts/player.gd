a7a56ef09eac3a2a0d3250ae46011227234a2b41
Julio <jantram710@g.educaand.es>
2024-12-04 17:47:38 +0100

    primer commit

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
+const SPEED =100.0
+const JUMP_VELOCITY = -350.0
+const WALL_SLIDING_SPEED = 1200
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -301.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") 
+var jumpsMade = 0
+var doWallJump = false
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process (delta):
+	var direction= Input.get_axis ("ui_left", "ui_right")
+	
+	if is_on_wall_only(): velocity.y = WALL_SLIDING_SPEED*delta
+	elif not is_on_floor(): velocity.y += gravity *	 delta
+	else: jumpsMade=0
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_wall_only(): 
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -direction * SPEED
+			doWallJump = true
+			$WallJumpTimer.start()
+		elif is_on_floor() || jumpsMade < 2:
+			velocity.y = JUMP_VELOCITY
+			jumpsMade += 1
+			
+	
+		
+	if direction && not doWallJump: velocity.x = direction * SPEED
+	elif not doWallJump: velocity.x = move_toward(velocity.x, 0,SPEED)
+	
 	move_and_slide()
+
+func _on_wall_jump_timer_timeout():
+	doWallJump= false
